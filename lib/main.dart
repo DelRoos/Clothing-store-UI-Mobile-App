2e3eaf6a38be686fb382bf7f6354462e82a786bf
DelRoos <delanoroosvelt733@gmail.com>
2023-03-04 12:14:05 +0100

    end hom page

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,394 @@
+import 'package:clothes_store_app_with_bottom_navigation/app_styles.dart';
+import 'package:clothes_store_app_with_bottom_navigation/size_config.dart';
+import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // const MyApp({super.key});
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: const HomeScreen(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          height: 64,
+          child: CustomNavigationBar(
+            isFloating: true,
+            borderRadius: const Radius.circular(40),
+            selectedColor: kWhite,
+            unSelectedColor: kGrey,
+            backgroundColor: kBrown,
+            strokeColor: Colors.transparent,
+            scaleFactor: 0.1,
+            iconSize: 40,
+            items: [
+              CustomNavigationBarItem(
+                icon: currentIndex == 0
+                    ? SvgPicture.asset(
+                        "assets/home_icon_selected.svg",
+                      )
+                    : SvgPicture.asset(
+                        "assets/home_icon_unselected.svg",
+                      ),
+              ),
+              CustomNavigationBarItem(
+                icon: currentIndex == 1
+                    ? SvgPicture.asset(
+                        "assets/cart_icon_selected.svg",
+                      )
+                    : SvgPicture.asset(
+                        "assets/cart_icon_unselected.svg",
+                      ),
+              ),
+              CustomNavigationBarItem(
+                icon: currentIndex == 2
+                    ? SvgPicture.asset(
+                        "assets/favorite_icon_selected.svg",
+                      )
+                    : SvgPicture.asset(
+                        "assets/favorite_icon_unselected.svg",
+                      ),
+              ),
+              CustomNavigationBarItem(
+                icon: currentIndex == 3
+                    ? SvgPicture.asset(
+                        "assets/account_icon_selected.svg",
+                      )
+                    : SvgPicture.asset(
+                        "assets/account_icon_unselected.svg",
+                      ),
+              ),
+            ],
+            currentIndex: currentIndex,
+            onTap: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> categories = [
+    "All Items",
+    "Dress",
+    "Hat",
+    "Watch",
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<String> icons = [
+    "all_items_icon",
+    "dress_icon",
+    "hat_icon",
+    "watch_icon",
+  ];
+
+  List<String> images = [
+    "image-01",
+    "image-02",
+    "image-03",
+    "image-04",
+    "image-02",
+    // "image-05",
+    // "image-06",
+  ];
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    SizeConfig.init(context);
+
+    return SafeArea(
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kPaddingHorizontal),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Hello, Welcome 👏',
+                      style: kEncodeSansRegular.copyWith(
+                        color: kDarkBrown,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                      ),
+                    ),
+                    Text(
+                      'Toungsi Roosvelt',
+                      style: kEncodeSansBold.copyWith(
+                        color: kDarkBrown,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 4,
+                      ),
+                    ),
+                  ],
+                ),
+                const CircleAvatar(
+                  radius: 20,
+                  backgroundColor: kGrey,
+                  backgroundImage: NetworkImage(
+                      'https://cdn.icon-icons.com/icons2/2468/PNG/512/user_user_thump_user_icon_user_profile_icon_149320.png'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: kPaddingHorizontal,
             ),
-          ],
-        ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    style: kEncodeSansRegular.copyWith(
+                      color: kDarkGrey,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                    ),
+                    controller: TextEditingController(),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 13,
+                      ),
+                      prefixIcon: const IconTheme(
+                        data: IconThemeData(color: kDarkGrey),
+                        child: Icon(
+                          Icons.search,
+                        ),
+                      ),
+                      hintText: "Search clothes...",
+                      border: kInputBorder,
+                      errorBorder: kInputBorder,
+                      disabledBorder: kInputBorder,
+                      focusedBorder: kInputBorder,
+                      focusedErrorBorder: kInputBorder,
+                      hintStyle: kEncodeSansRegular.copyWith(
+                        color: kDarkGrey,
+                        fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Container(
+                  height: 49,
+                  width: 49,
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(kBorderRadius),
+                    color: kBlack,
+                  ),
+                  child: SvgPicture.asset('assets/filter_icon.svg'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 36,
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: categories.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      left: index == 0 ? kPaddingHorizontal : 15,
+                      right: index == categories.length - 1
+                          ? kPaddingHorizontal
+                          : 0,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                    ),
+                    height: 36,
+                    decoration: BoxDecoration(
+                      color: currentIndex == index ? kBrown : kWhite,
+                      borderRadius: BorderRadius.circular(8),
+                      border: currentIndex == index
+                          ? null
+                          : Border.all(
+                              color: kLightGrey,
+                              width: 1,
+                            ),
+                    ),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          currentIndex == index
+                              ? "assets/${icons[index]}_selected.svg"
+                              : "assets/${icons[index]}_unselected.svg",
+                        ),
+                        const SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          categories[index],
+                          style: kEncodeSansMedium.copyWith(
+                            color: currentIndex == index ? kWhite : kDarkBrown,
+                            fontSize: SizeConfig.blockSizeHorizontal! * 3,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          MasonryGridView.count(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            crossAxisCount: 2,
+            itemCount: images.length,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 23,
+            padding: const EdgeInsets.symmetric(
+              horizontal: kPaddingHorizontal,
+            ),
+            itemBuilder: (context, index) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Positioned(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(kBorderRadius),
+                          child: Image.asset(
+                            "assets/images/${images[index]}.png",
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 12,
+                        top: 12,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: SvgPicture.asset(
+                            "assets/favorite_cloth_icon_unselected.svg",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "Modern light clothes",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: kEncodeSansSemiBold.copyWith(
+                      color: kDarkBrown,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                    ),
+                  ),
+                  Text(
+                    "Dress modern",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: kEncodeSansRegular.copyWith(
+                      color: kGrey,
+                      fontSize: SizeConfig.blockSizeHorizontal! * 2.5,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "\$121.98",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: kEncodeSansSemiBold.copyWith(
+                          color: kDarkBrown,
+                          fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            color: kYellow,
+                            size: 16,
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            "5.0",
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: kEncodeSansRegular.copyWith(
+                              color: kGrey,
+                              fontSize: SizeConfig.blockSizeHorizontal! * 3,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
